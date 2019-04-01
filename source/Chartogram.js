5e6bfb95ed42bac097f451ed036554a19c6e2911
catamphetamine <purecatamphetamine@gmail.com>
2019-04-01 14:17:40 +0300

    Togglers.js

--- source/Chartogram.js
+++ source/Chartogram.js
@@ -8,6 +8,7 @@ import {
 } from './utility'
 
 import Timeline from './Timeline'
+import Togglers from './Togglers'
 
 export default class Chartogram {
 	constructor(rootNode, data, title = 'Title', props = {}) {
@@ -77,7 +78,7 @@ export default class Chartogram {
 				</div>
 			</div>
 			${Timeline.INITIAL_MARKUP}
-			<div class="chartogram__chart-togglers"></div>
+			${Togglers.INITIAL_MARKUP}
 		`
 
 		this.tooltipContainer = this.rootNode.querySelector('.chartogram__plan')
@@ -88,18 +89,14 @@ export default class Chartogram {
 
 		this.setUpCanvasTooltipListener()
 
-		// Add graph togglers.
-		const graphTogglers = this.rootNode.querySelector('.chartogram__chart-togglers')
-		clearElement(graphTogglers)
-		for (const y of this.data.y) {
-			graphTogglers.appendChild(this.createGraphToggler(y))
-		}
-
 		// Render will be called after `.componentDidMount()`.
 		this.state = this.getInitialState()
 
 		this.timeline = new Timeline(this.getTimelineProps())
 		this.timeline.componentDidMount()
+
+		this.togglers = new Togglers(this.getTogglersProps())
+		this.togglers.componentDidMount()
 
 		// Add window resize event listener.
 		window.addEventListener('resize', this.onResizeThrottled)
@@ -151,6 +148,29 @@ export default class Chartogram {
 			y: this.state.y,
 			yScale: this.state.yScale,
 			onChangeBounds: this.onChangeBounds
+		}
+	}
+
+	getTogglersProps() {
+		return {
+			rootNode: this.rootNode,
+			data: this.data,
+			onToggle: (id) => {
+				const y = this.state.y.find(_ => _.id === id)
+				// Won't allow hiding all graphs.
+				if (y.isShown) {
+					const graphsShown = this.state.y.filter(_ => _.isShown)
+					if (graphsShown.length === 1) {
+						return
+					}
+				}
+				y.isShown = !y.isShown
+				this.setState({
+					y: this.state.y,
+					...this.calculateMinMaxY(this.state.y)
+				})
+				return true
+			}
 		}
 	}
 
@@ -384,61 +404,6 @@ export default class Chartogram {
 		this.yAxis.style.height = `${100 / yAxisScale}%`
 	}
 
-	createGraphToggler = ({ id, name, color }) => {
-		const toggler = document.createElement('button')
-		toggler.setAttribute('type', 'button')
-		toggler.classList.add('chartogram__chart-toggler')
-		toggler.classList.add('chartogram__chart-toggler--on')
-		toggler.classList.add('chartogram__reset-button')
-		// Add check.
-		const xmlns = 'http://www.w3.org/2000/svg'
-		const check = document.createElementNS(xmlns, 'svg')
-		check.setAttribute('viewBox', '0 0 19 19')
-		check.setAttribute('class', 'chartogram__chart-toggler-check')
-		// Add background circle.
-		const backgroundCircle = document.createElementNS(xmlns, 'circle')
-		backgroundCircle.setAttribute('cx', '9.5')
-		backgroundCircle.setAttribute('cy', '9.5')
-		backgroundCircle.setAttribute('r', '9.5')
-		backgroundCircle.setAttribute('fill', color)
-		check.appendChild(backgroundCircle)
-		// Add check circle.
-		const checkCircle = document.createElementNS(xmlns, 'circle')
-		checkCircle.setAttribute('cx', '9.5')
-		checkCircle.setAttribute('cy', '9.5')
-		checkCircle.setAttribute('r', '8')
-		checkCircle.setAttribute('class', 'chartogram__chart-toggler-check-circle')
-		check.appendChild(checkCircle)
-		// Add check mark.
-		const checkMark = document.createElementNS(xmlns, 'path')
-		checkMark.setAttribute('d', 'M13.64 4.94l-6.2 6.34-1.69-1.9c-.73-.63-1.89.1-1.36 1.06l2 3.38c.3.43 1.04.85 1.78 0 .32-.42 6.31-7.93 6.31-7.93.74-.84-.2-1.58-.84-.95z')
-		checkMark.setAttribute('fill', 'white')
-		checkMark.setAttribute('class', 'chartogram__chart-toggler-check-mark')
-		check.appendChild(checkMark)
-		// Add checkmark.
-		toggler.appendChild(check)
-		// Add graph name.
-		toggler.appendChild(document.createTextNode(name))
-		// On click.
-		toggler.addEventListener('click', () => {
-			const y = this.state.y.find(_ => _.id === id)
-			// Won't allow hiding all graphs.
-			if (y.isShown) {
-				const graphsShown = this.state.y.filter(_ => _.isShown)
-				if (graphsShown.length === 1) {
-					return
-				}
-			}
-			y.isShown = !y.isShown
-			this.setState({
-				...this.state,
-				...this.calculateMinMaxY(this.state.y)
-			})
-			toggler.classList.toggle('chartogram__chart-toggler--on')
-		})
-		return toggler
-	}
-
 	setUpCanvasTooltipListener = () => {
 		const { weekdays, months } = this.props
 		let canvasDimensions
@@ -646,12 +611,16 @@ export default class Chartogram {
 			this.addTooltipPoints()
 		}
 		let i = 0
-		while (i < this.tooltipPoints.length) {
-			const point = this.tooltipPoints[i]
-			const _y = y[i].points[xIndex]
-			const yRatio = _y / maxY
-			point.style.left = `${xRatio * 100}%`
-			point.style.bottom = `${yRatio * 100}%`
+		let j = 0
+		while (i < y.length) {
+			if (y[i].isShown) {
+				const point = this.tooltipPoints[j]
+				const _y = y[i].points[xIndex]
+				const yRatio = _y / maxY
+				point.style.left = `${xRatio * 100}%`
+				point.style.bottom = `${yRatio * 100}%`
+				j++
+			}
 			i++
 		}
 	}
---
+++ source/Togglers.js
@@ -0,0 +1,70 @@
+import {
+	clearElement
+} from './utility'
+
+export default class Togglers {
+	constructor(props) {
+		this.props = props
+	}
+
+	componentDidMount() {
+		const { rootNode, data } = this.props
+		// Add graph togglers.
+		const graphTogglers = rootNode.querySelector('.chartogram__chart-togglers')
+		clearElement(graphTogglers)
+		for (const y of data.y) {
+			graphTogglers.appendChild(this.createGraphToggler(y))
+		}
+	}
+
+	createGraphToggler = ({ id, name, color }) => {
+		const toggler = document.createElement('button')
+		toggler.setAttribute('type', 'button')
+		toggler.classList.add('chartogram__chart-toggler')
+		toggler.classList.add('chartogram__chart-toggler--on')
+		toggler.classList.add('chartogram__reset-button')
+		// Add check.
+		const xmlns = 'http://www.w3.org/2000/svg'
+		const check = document.createElementNS(xmlns, 'svg')
+		check.setAttribute('viewBox', '0 0 19 19')
+		check.setAttribute('class', 'chartogram__chart-toggler-check')
+		// Add background circle.
+		const backgroundCircle = document.createElementNS(xmlns, 'circle')
+		backgroundCircle.setAttribute('cx', '9.5')
+		backgroundCircle.setAttribute('cy', '9.5')
+		backgroundCircle.setAttribute('r', '9.5')
+		backgroundCircle.setAttribute('fill', color)
+		check.appendChild(backgroundCircle)
+		// Add check circle.
+		const checkCircle = document.createElementNS(xmlns, 'circle')
+		checkCircle.setAttribute('cx', '9.5')
+		checkCircle.setAttribute('cy', '9.5')
+		checkCircle.setAttribute('r', '8')
+		checkCircle.setAttribute('class', 'chartogram__chart-toggler-check-circle')
+		check.appendChild(checkCircle)
+		// Add check mark.
+		const checkMark = document.createElementNS(xmlns, 'path')
+		checkMark.setAttribute('d', 'M13.64 4.94l-6.2 6.34-1.69-1.9c-.73-.63-1.89.1-1.36 1.06l2 3.38c.3.43 1.04.85 1.78 0 .32-.42 6.31-7.93 6.31-7.93.74-.84-.2-1.58-.84-.95z')
+		checkMark.setAttribute('fill', 'white')
+		checkMark.setAttribute('class', 'chartogram__chart-toggler-check-mark')
+		check.appendChild(checkMark)
+		// Add checkmark.
+		toggler.appendChild(check)
+		// Add graph name.
+		toggler.appendChild(document.createTextNode(name))
+		// On click.
+		toggler.addEventListener('click', () => this.onToggle(id, toggler))
+		return toggler
+	}
+
+	onToggle = (id, toggler) => {
+		const { onToggle } = this.props
+		if (onToggle(id)) {
+			toggler.classList.toggle('chartogram__chart-toggler--on')
+		}
+	}
+}
+
+Togglers.INITIAL_MARKUP = `
+	<div class="chartogram__chart-togglers"></div>
+`
